6332d012bb8bac61adea9292ae1e813dec2ef04b
Sam Kreter <samkreter@gmail.com>
2016-09-05 16:23:12 -0500

    fixed the serilization in ajax call

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -58,7 +58,7 @@ end
 
 
 # heroku setup
-# heroku config:set S3_BUCKET_NAME=your_bucket_name
-# heroku config:set AWS_ACCESS_KEY_ID=your_access_key_id
-# heroku config:set AWS_SECRET_ACCESS_KEY=your_secret_access_key
-# heroku config:set AWS_REGION=your_aws_region
+# heroku config:set S3_BUCKET_NAME=resume16
+# heroku config:set AWS_ACCESS_KEY_ID=AKIAJSFCV66627WSDHBQ
+# heroku config:set AWS_SECRET_ACCESS_KEY=xf2lCBd4jUa6ZvX1UX1FR4EXEDUQh9aDUkOmlVRP
+# heroku config:set AWS_REGION=Oregon
